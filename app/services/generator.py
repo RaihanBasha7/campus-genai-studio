ef75f95005ad077a4c61af038d077c5003959725
Raihanshaik7 <raihanshaik676@gmail.com>
2026-02-22 18:00:25 +0530

    Initial FastAPI backend with generate endpoint

---
+++ app/services/generator.py
@@ -0,0 +1,12 @@
+def generate_event_plan(idea: str):
+    
+    return {
+        "event_name": "Tech Fest",
+        "tasks": [
+            "Posters",
+            "Registrations",
+            "Venue Booking",
+            "Sponsors"
+        ],
+        "timeline": "2 weeks"
+    }
